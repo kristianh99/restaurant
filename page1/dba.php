677f984d4e5d2f40c9b82bc053dedb6bef9a1d8d
Kristian Horvat <65367442+kristianh99@users.noreply.github.com>
2020-08-25 15:46:48 +0200

    Update dba.php

--- page1/dba.php
+++ page1/dba.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect('localhost', 'root', '', 'test');
+$conn=mysqli_connect('localhost', 'root', '', 'restaurant');
 
 if(!$conn){
     die("Connection filed: ".mysqli_connect_error());
