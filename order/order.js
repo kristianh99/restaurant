75459b8be0decc19dbe09af3033c6dc3e7e18068
Kristian Horvat <65367442+kristianh99@users.noreply.github.com>
2020-08-30 15:02:21 +0200

    dovrseno

--- order/order.js
+++ order/order.js
@@ -11,6 +11,23 @@ for(var i=0; i < addToCartButtons.length; i++){
     button.addEventListener('click', addToCartClicked)
 }
 
+var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+}
+
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
+function purchaseClicked() {
+    alert('Vaša porudžbina je poslata!')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
 function addToCartClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
@@ -40,7 +57,7 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" type="button">OBRIŠI</button>
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
@@ -51,6 +68,14 @@ function addItemToCart(title, price, imageSrc) {
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
     updateCartTotal()
 }
 
@@ -67,5 +92,5 @@ function updateCartTotal() {
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText =  total + 'din'
+    document.getElementsByClassName('cart-total-price')[0].innerText =  total + ' din'
 }
